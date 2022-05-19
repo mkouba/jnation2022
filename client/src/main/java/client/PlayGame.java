05bcc56dd7f2b9c0934f17abad461d9e8f38a959
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2022-05-19 14:30:12 +0200

    failure handling for the incoming stream

--- client/src/main/java/client/PlayGame.java
+++ client/src/main/java/client/PlayGame.java
@@ -1,5 +1,6 @@
 package client;
 
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import com.github.mkouba.jn22.ExerciseResult;
@@ -8,12 +9,15 @@ import com.github.mkouba.jn22.JoinRequest;
 
 import com.github.mkouba.jn22.JoinResult;
 import io.quarkus.grpc.GrpcClient;
+import io.quarkus.logging.Log;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.subscription.MultiEmitter;
 import io.vertx.core.Vertx;
+import org.jboss.logging.Logger;
 
 @QuarkusMain
 public class PlayGame implements QuarkusApplication {
@@ -23,6 +27,12 @@ public class PlayGame implements QuarkusApplication {
     
     @Inject
     Vertx vertx;
+
+    volatile boolean shutdown;
+
+    void shutdown(@Observes ShutdownEvent event) {
+        shutdown = true;
+    }
 
     @Override
     public int run(String... args) {
@@ -35,12 +45,18 @@ public class PlayGame implements QuarkusApplication {
 
             String token = r.getToken();
             logHeader(r);
-            Multi<ExerciseResult> multi = Multi.createFrom().emitter(
+            Multi<ExerciseResult> multi = Multi.createFrom().<ExerciseResult>emitter(
                     em -> vertx.executeBlocking(promise -> gameLoop(token, em))
+            ).onFailure().invoke(error ->
+                Log.log(org.jboss.logging.Logger.Level.ERROR, error)
             );
 
             game.play(multi).subscribe().with(sm -> {
                 System.out.println(sm.getText());
+            }, th -> {
+                if (!shutdown) {
+                    Log.log(Logger.Level.ERROR, "Failure on message consumption", th);
+                }
             });
             
         });
@@ -57,7 +73,7 @@ public class PlayGame implements QuarkusApplication {
     }
 
     private void gameLoop(String token, MultiEmitter<? super ExerciseResult> em) {
-        while (true) {
+        while (!shutdown) {
             String resultStr = "";
             try {
                 resultStr = System.console().readLine().trim();
